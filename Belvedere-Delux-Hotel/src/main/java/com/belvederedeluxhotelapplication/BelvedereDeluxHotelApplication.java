fdd42ee9d111a2c34421e6d5e46f6acde1515c9a
jawad abo fakher <aboufakherj@gmail.com>
2023-11-18 19:24:48 +0200

    Commiting src file
    Uploading the source file to the remote repository - github

---
+++ Belvedere-Delux-Hotel/src/main/java/com/belvederedeluxhotelapplication/BelvedereDeluxHotelApplication.java
@@ -0,0 +1,13 @@
+package com.belvederedeluxhotelapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelvedereDeluxHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelvedereDeluxHotelApplication.class, args);
+	}
+
+}
---
+++ Belvedere-Delux-Hotel/src/test/java/com/belvederedeluxhotelapplication/BelvedereDeluxHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belvederedeluxhotelapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelvedereDeluxHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
