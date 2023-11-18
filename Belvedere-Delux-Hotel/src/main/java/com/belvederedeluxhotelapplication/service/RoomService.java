e59dc5ab231d4ee15b16aae04f8d56a378f2617a
jawad abo fakher <aboufakherj@gmail.com>
2023-11-18 20:23:06 +0200

    updates tack -roomservice

--- Belvedere-Delux-Hotel/src/main/java/com/belvederedeluxhotelapplication/service/RoomService.java
+++ Belvedere-Delux-Hotel/src/main/java/com/belvederedeluxhotelapplication/service/RoomService.java
@@ -1,2 +1,16 @@
-package com.belvederedeluxhotelapplication.service;public interface RoomService {
+package com.belvederedeluxhotelapplication.service;
+
+import com.belvederedeluxhotelapplication.model.Room;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+
+@Service
+public interface RoomService {
+
+    Room addNewRoom(String roomType, BigDecimal roomPrice, MultipartFile photo) throws IOException, SQLException;
+
 }
